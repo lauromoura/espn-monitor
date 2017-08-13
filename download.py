fa6f5fabf4bb7ce0c2c64363acdd1fae5cc44455
Lauro Moura <lauromoura@gmail.com>
2017-08-13 16:17:17 -0300

    Start adding arguments for arbritary dates

--- download.py
+++ download.py
@@ -1,7 +1,8 @@
 """Download module to get tv schedules"""
 
 
-from datetime import date
+import argparse
+from datetime import date, datetime
 
 import pandas as pd
 
@@ -9,14 +10,34 @@ from parsers import ESPNFetcher
 
 
 
+ARGUMENT_DATE = '%Y-%m-%d'
+
+
+def valid_date(string):
+    try:
+        return datetime.strptime(string, ARGUMENT_DATE).date()
+    except ValueError:
+        msg = 'Not a valid date: "{0}".'.format(string)
+        raise argparse.ArgumentTypeError(msg)
+
+
 def main():
     '''Donwloads and generates csv file for the tv schedule'''
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--start', help='First date to fetch',
+                        required=False, default=date.today(),
+                        type=valid_date)
+
+    args = parser.parse_args()
+
+    print(args)
+
     fetcher = ESPNFetcher()
 
-    channels_data = fetcher.get(target_date=date.today())
+    channels_data = fetcher.get(args.start)
 
-    channels_data.to_csv('%s-%s.csv' % (fetcher.name(), date.today()), encoding='utf-8')
+    channels_data.to_csv('%s-%s.csv' % (fetcher.name(), args.start), encoding='utf-8')
 
 
 if __name__ == '__main__':
